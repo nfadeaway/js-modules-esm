a2c221da37fe2af22ec257421ef9b931d86c6bcf
Evgeniy Degtyar <farappl@gmail.com>
2023-06-27 23:29:08 +0500

    task 3

---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+class Character {
+}
+
+class Game {
+  start() {
+    console.log('game started');
+  }
+}
+
+class GameSavingData {
+}
+
+function readGameSaving() {
+}
+
+function writeGameSaving() {
+}
+
+console.log('app worked');
+
+const game = new Game();
+game.start();
